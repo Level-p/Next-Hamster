e03fdd34c92914784462f4257cd9649f1e4e160b
Steven <onojasteven13@gmail.com>
2024-04-19 23:15:49 +0100

    work

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -10,7 +10,7 @@ useEffect(() => {
 }, [error])
   return (
     <div className="text-center my-10 space-y-6">
-        <h1>Something went wrong, Please try again later.</h1>
+        <h1 className="font-bold text-2xl">Something went wrong, Please try again later.</h1>
         <button className="transition-all duration-200 hover:text-amber-600 font-semibold p-2" onClick={() => reset()}>Try Again</button>
     </div>
   )
