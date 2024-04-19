1c2baa3fab04af8bda73eaf979debcb595e8404f
Steven <onojasteven13@gmail.com>
2024-04-19 23:43:45 +0100

    sxsx

--- src/components/design/Navbar.jsx
+++ src/components/design/Navbar.jsx
@@ -1,12 +1,16 @@
+"juse client"
+import { Suspense } from "react";
 import NavbarItem from "../NavbarItem";
 import Pages from "../Pages";
 
 export default function Navbar() {
   return (
-    <div className="flex dark:bg-slate-950 items-center justify-evenly md:justify-center gap-4 md:gap-12  bg-slate-100 dark:text-white p-4">
+    <Suspense >
+      <div className="flex dark:bg-slate-950 items-center justify-evenly md:justify-center gap-4 md:gap-12  bg-slate-100 dark:text-white p-4">
         <NavbarItem title="Trending" param="fetchTrending"/> 
         <NavbarItem title="Top Rated" param="fetchTopRated"/> 
         <Pages title="Anime" param="anime"/> 
-    </div>
+        </div>
+    </Suspense>
   )
 }
