cfde24b15ab2655c9dddeb7b5bbfcbef8e5a5b90
Steven <onojasteven13@gmail.com>
2024-05-08 15:51:04 +0100

    sent

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['image.tmdb.org', 'tmssl.akamaized.net', 'cdn.myanimelist.net', 'images.unsplash.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'image.tmdb.org',
+                pathname: '**'
+            },
+            {
+                protocol: 'https',
+                hostname: 'tmssl.akamaized.net',
+                pathname: '**'
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn.myanimelist.net',
+                pathname: '**'
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                pathname: '**'
+            }
+        ]
     }
     
 };
